6ff83f8ae85a9faa70dd620acf1d2ae1268c5349
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-07-12 11:40:08 +0200

    Scrutinizer Auto-Fixes (#3)
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- action.php
+++ action.php
@@ -7,18 +7,18 @@
  * @author     Marius Rieder <marius.rieder@durchmesser.ch>
  */
 
-if(!defined('DOKU_INC')) die();
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+if (!defined('DOKU_INC')) die();
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'action.php');
 
 class action_plugin_webmaster extends DokuWiki_Action_Plugin {
 
     function register(Doku_Event_Handler $controller) {
-        $controller->register_hook('TPL_METAHEADER_OUTPUT','BEFORE',$this,'addVerifyHeaders',array());
+        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'addVerifyHeaders', array());
     }   
 
     function addVerifyHeaders(&$event, $param) {
-        if(empty($event->data)||empty($event->data['meta'])) return;
+        if (empty($event->data)||empty($event->data['meta'])) return;
 
         /* Google */
         $g = $this->getConf('webmaster_google');
